a57849c874f093e8999d9697f0d2077385238d3e
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-10-06 22:16:46 -0700

    created title component, working on questionBox

---
+++ src/Components/QuestionBox/QuestionBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuestionBox = _ => {
+    return (
+        <h1>Question Box</h1>
+    )
+}
+
+export default QuestionBox
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+const Title = _ => {
+  return (
+    <div id="trivia_div">
+      <h1 id="trivia_title">SAVED BY THE BELL</h1>
+    </div>
+  );
+};
+
+export default Title;
